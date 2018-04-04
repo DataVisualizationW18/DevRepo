c32d7c61a203b02e1b4f77a79b5a25dbe6abdf7b
sehoven <shoven@ualberta.ca>
2018-04-03 23:11:23 -0600

    Added disabling to save button when no libraries or metrics are selected.

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def handle_data():
 
     invisBackground_style = Style(background='transparent')
 
-    line_chart = pygal.Bar(style = invisBackground_style)
+    line_chart = pygal.Bar(style = invisBackground_style, width=600)
     line_chart.title = 'Browser usage evolution (in %)'
     line_chart.x_labels = map(str, range(2002, 2013))
     line_chart.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
@@ -51,9 +51,18 @@ def handle_data():
     line_chart.add('Others',  [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
     chart = line_chart.render_data_uri()
 
+    line_chart2 = pygal.Bar(style = invisBackground_style, width=1200)
+    line_chart2.title = 'Browser usage evolution (in %)'
+    line_chart2.x_labels = map(str, range(2002, 2013))
+    line_chart2.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    line_chart2.add('Chrome',  [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
+    line_chart2.add('IE',      [85.8, 84.6, 84.7, 74.5, 66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    line_chart2.add('Others',  [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
+    chart2 = line_chart2.render_data_uri()
+
     charts = []
     charts.append(chart)
     charts.append(chart)
-    charts.append(chart)
+    charts.append(chart2)
 
     return jsonify(charts)
