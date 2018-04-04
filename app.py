6e5179c4b7674f516a48f8b5e281484759748e9e
sehoven <shoven@ualberta.ca>
2018-04-03 19:43:06 -0600

    Passing data back to flask from index.html.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pygal
 from pygal.style import Style
 app = Flask(__name__)
@@ -19,8 +19,7 @@ def hello_world(name=None):
         "xml": ["xerces2-j","dom4j","jdom"]
     }
 
-    invisBackground_style = Style(
-    background='transparent')
+    invisBackground_style = Style(background='transparent')
 
     line_chart = pygal.Bar(style = invisBackground_style)
     line_chart.title = 'Browser usage evolution (in %)'
@@ -33,3 +32,31 @@ def hello_world(name=None):
 
     return render_template('index.html',domain_list=domain_list, chart = chart)
 
+
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    domain_list = {
+        "": [],
+        "cryptography": ["bouncycastle","commons crypto","conceal","chimera","spongycastle","keyczar","conscrypt"],
+        "databases": ["h2","derby"],
+        "json": ["gson","json.simple"],
+        "logging": ["slf4j","log4j2","logback","commons logging","tinylog","blitz4j","minlog"],
+        "mocking": ["mockito","easymock","powermock","jmock"],
+        "object-relational-mapping": ["hibernate orm","mybatis3","ormlite"],
+        "security": ["shiro","spring security"],
+        "testing": ["junit4","testng"],
+        "utilities": ["guava","commons lang"],
+        "xml": ["xerces2-j","dom4j","jdom"]
+    }
+
+    invisBackground_style = Style(background='transparent')
+
+    line_chart = pygal.Bar(style = invisBackground_style)
+    line_chart.title = 'Browser usage evolution (in %)'
+    line_chart.x_labels = map(str, range(2002, 2013))
+    line_chart.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    line_chart.add('Chrome',  [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
+    chart = line_chart.render_data_uri()
+
+    print(request.json)
+    return render_template('index.html', domain_list=domain_list, chart=chart)
