a1598459c64c7bef4dfac01f2c7e652dc58cd283
sehoven <shoven@ualberta.ca>
2018-04-03 20:55:36 -0600

    Fixed some styling and the dynamic grid.

--- app.py
+++ app.py
@@ -19,21 +19,10 @@ def hello_world(name=None):
         "xml": ["xerces2-j","dom4j","jdom"]
     }
 
-    invisBackground_style = Style(background='transparent')
-
-    line_chart = pygal.Bar(style = invisBackground_style)
-    line_chart.title = 'Browser usage evolution (in %)'
-    line_chart.x_labels = map(str, range(2002, 2013))
-    line_chart.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    line_chart.add('Chrome',  [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
-    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5, 66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    line_chart.add('Others',  [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
-    chart = line_chart.render_data_uri()
-
-    return render_template('index.html',domain_list=domain_list, chart = chart)
+    return render_template('index.html',domain_list=domain_list)
 
 
-@app.route('/handle_data', methods=['POST'])
+@app.route('/generate_chart', methods=['POST'])
 def handle_data():
     domain_list = {
         "": [],
@@ -49,6 +38,8 @@ def handle_data():
         "xml": ["xerces2-j","dom4j","jdom"]
     }
 
+    print(request.json)
+
     invisBackground_style = Style(background='transparent')
 
     line_chart = pygal.Bar(style = invisBackground_style)
@@ -56,7 +47,13 @@ def handle_data():
     line_chart.x_labels = map(str, range(2002, 2013))
     line_chart.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
     line_chart.add('Chrome',  [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
+    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5, 66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    line_chart.add('Others',  [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
     chart = line_chart.render_data_uri()
 
-    print(request.json)
-    return render_template('index.html', domain_list=domain_list, chart=chart)
+    charts = []
+    charts.append(chart)
+    charts.append(chart)
+    charts.append(chart)
+
+    return jsonify(charts)
