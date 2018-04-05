9c9329c46942a3d1ea9f9807f187c42f8afe429c
Tyler Heise <theise@cs.ualberta.ca>
2018-04-05 16:02:58 -0600

    fixed generate one func

--- app.py
+++ app.py
@@ -162,6 +162,7 @@ def handle_data():
         else:
             chart = generate(lib_list, metric['metric'], metric['chart_type'])
             vis_type = 'chart'
+            chart = chart.render_data_uri()
 
         def_chart= metric['chart_type']
         if def_chart == 'default':
@@ -169,57 +170,11 @@ def handle_data():
 
         chart_dict = {'metric':metric['metric'],
         'type':vis_type,
-        'data': chart.render_data_uri(),
+        'data': chart,
         'chart_type': def_chart
         }
 
         charts.append(chart_dict)
-
-    # invisBackground_style = Style(background='transparent')
-
-    # line_chart = pygal.Bar(style = invisBackground_style, width=600)
-    # line_chart.title = 'Browser usage evolution (in %)'
-    # line_chart.x_labels = map(str, range(2002, 2013))
-    # line_chart.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    # line_chart.add('Chrome',  [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
-    # line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5, 66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    # line_chart.add('Others',  [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
-    # chart = line_chart.render_data_uri()
-
-    # line_chart2 = pygal.Bar(style = invisBackground_style, width=1200)
-    # line_chart2.title = 'Browser usage evolution (in %)'
-    # line_chart2.x_labels = map(str, range(2002, 2013))
-    # line_chart2.add('Firefox', [None, None, 0, 16.6, 25, 31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    # line_chart2.add('Chrome',  [None, None, None, None, None, None, 0, 3.9, 10.8, 23.8, 35.3])
-    # line_chart2.add('IE',      [85.8, 84.6, 84.7, 74.5, 66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    # line_chart2.add('Others',  [14.2, 15.4, 15.3, 8.9, 9, 10.4, 8.9, 5.8, 6.7, 6.8, 7.5])
-    # chart2 = line_chart2.render_data_uri()
-
-    # charts = []
-    # charts.append({
-    #     "metric": "popularity",
-    #     "type": "chart",
-    #     "data": chart,
-    #     "chart_type": "bar_raw"
-    # });
-    # charts.append({
-    #     "metric": "performance",
-    #     "type": "chart",
-    #     "data": chart,
-    #     "chart_type": "box"
-    # });
-    # charts.append({
-    #     "metric": "issue-closing-time",
-    #     "type": "chart",
-    #     "data": chart2,
-    #     "chart_type": "xy"
-    # });
-    # charts.append({
-    #     "metric": "security",
-    #     "type": "raw_data",
-    #     "data": "",
-    #     "chart_type": "raw_data"
-    # });
 
     return jsonify(charts)
 
@@ -266,9 +221,21 @@ def handle_one_data():
         ]
     }
 
+
+    metric_dict = request.json
+    default_dict={'popularity': 'bar_raw',
+                    'release-frequency':'bar_avg',
+                    'last-modification-date':'bar_days',
+                    'performance':'box',
+                    'security':'box',
+                    'issue-response-time':'xy',
+                    'issue-closing-time':'xy',
+                    'backwards-compatibility':'bar',
+                    'last-discussed-on-so':'box'}
+
     metric_dict = request.json
     print(metric_dict)
-    metric = metric_dict['metrics']
+    metric = metric_dict['metric']
 
     lib_list=[]
     for domain in parsed_domains:
@@ -284,6 +251,7 @@ def handle_one_data():
     else:
         chart = generate(lib_list, metric['metric'], metric['chart_type'])
         vis_type = 'chart'
+        chart = chart.render_data_uri()
 
     def_chart= metric['chart_type']
     if def_chart == 'default':
@@ -291,7 +259,7 @@ def handle_one_data():
 
     chart_dict = {'metric':metric['metric'],
         'type':vis_type,
-        'data': chart.render_data_uri(),
+        'data': chart,
         'chart_type': def_chart
         }
 
